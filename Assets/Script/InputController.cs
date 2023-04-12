aaaa00c7380edf06512c44048b1410649f07b81d
Mforce22 <matte.guardi@gmail.com>
2023-04-12 13:33:57 +0200

    creato il campo
    e aggiunto lo sprite frogger

--- Assets/Script/InputController.cs
+++ Assets/Script/InputController.cs
@@ -19,7 +19,7 @@ public class InputController : MonoBehaviour
     // Start is called before the first frame update ef
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
